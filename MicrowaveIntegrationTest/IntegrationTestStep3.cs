7ed55be5b675146803a7ca6d198001004ac34e37
Kasper Fogh Jensen <201602344@post.au.dk>
2020-04-26 14:05:17 +0200

    oprettet klasser til integrationstest af step3 og 6. attributter og setup oprettet

---
+++ MicrowaveIntegrationTest/IntegrationTestStep3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+
+
+namespace MicrowaveIntegrationTest
+{
+    [TestFixture]
+    public class IntegrationTestStep3
+    {
+        private CookController _uut;
+        private IUserInterface _iUserInterface;
+        private ITimer _iTimer;
+        private IPowerTube _iPowerTube;
+        private IOutput _iOutput;
+        private Display _display;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _iUserInterface = Substitute.For<IUserInterface>();
+            _iTimer = Substitute.For<ITimer>();
+            _iPowerTube = Substitute.For<IPowerTube>();
+            _iOutput = Substitute.For<IOutput>();
+            _display = new Display(_iOutput);
+            _uut = new CookController(_iTimer, _display, _iPowerTube, _iUserInterface);
+        }
+
+
+
+    }
+}
