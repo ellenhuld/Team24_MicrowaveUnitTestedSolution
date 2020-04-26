c7320ed6b59594079f79838e577cf57234725d99
Rune Matthias Hell <au550573@uni.au.dk>
2020-04-27 01:57:31 +0200

    Step 1 færdigt rettelser i Timer samt TimerTest

--- MicrowaveIntegrationTest/IntegrationTestStep1.cs
+++ MicrowaveIntegrationTest/IntegrationTestStep1.cs
@@ -31,22 +31,79 @@ namespace MicrowaveIntegrationTest
             _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
         }
 
-        [TestCase(6,1000,0,5)]
-        [TestCase(120, 1000, 1,59)]
-        [TestCase()]
-        //[TestCase()]
-        public void TimerTick(int time, int sleeptime, int showMin, int showSec )
+        // Fail compares to the order in testcases...why? alone test they succeeded
+        [TestCase(120, 1000, 1, 59)]
+        [TestCase(6,1000,0,5)] // fail sometimes
+        [TestCase(120, 2000, 1, 58)]// Shouldn't fail (doesn't fail everytime)
+        public void TimerTick_On_Display_ShowTime(int time, int sleepTime, int showMin, int showSec )
         {
             int power = 700;
             _cookController.StartCooking(power,time);
-            Thread.Sleep(sleeptime);
-            _display.Received().ShowTime(showMin, showSec);
+            
+            Thread.Sleep(sleepTime);
+            _display.Received(1).ShowTime(showMin, showSec);
         }
 
-        [TestCase()]
-        public void Expired()
+        [TestCase(120, 1000, 1, 59)]
+        public void TimerTick_On_Display_ShowTime1(int time, int sleepTime, int showMin, int showSec)
         {
+            int power = 700;
+            _cookController.StartCooking(power, time);
 
+            Thread.Sleep(sleepTime);
+            _display.Received(1).ShowTime(showMin, showSec);
         }
+
+        [TestCase(6, 1000, 0, 5)]
+        public void TimerTick_On_Display_ShowTime2(int time, int sleepTime, int showMin, int showSec)
+        {
+            int power = 700;
+            _cookController.StartCooking(power, time);
+
+            Thread.Sleep(sleepTime);
+            _display.Received(1).ShowTime(showMin, showSec);
+        }
+        [TestCase(120, 2000, 1, 58)]
+        public void TimerTick_On_Display_ShowTime3(int time, int sleepTime, int showMin, int showSec)
+        {
+            int power = 700;
+            _cookController.StartCooking(power, time);
+
+            Thread.Sleep(sleepTime);
+            _display.Received(1).ShowTime(showMin, showSec);
+        }
+        
+
+        // Again problems when it goes in the testcases
+        [TestCase(2,2000)]
+        [TestCase(4,4000)]
+        public void Timer_Expired_CookingIsDone(int time, int sleepTime)
+        {
+            int power = 700;
+            _cookController.StartCooking(power,time);
+            
+            Thread.Sleep(sleepTime);
+            _userInterface.Received(1).CookingIsDone();
+        }
+        [TestCase(2, 1000)]
+        [TestCase(4, 4000)]
+        public void Timer_Not_Expired_No_CookingIsDone(int time, int sleepTime)
+        {
+            int power = 700;
+            _cookController.StartCooking(power, time);
+
+            Thread.Sleep(sleepTime);
+            _userInterface.DidNotReceive().CookingIsDone();
+        }
+        [TestCase(4, 4000)]
+        public void Timer_Not_Expired_No_CookingIsDone1(int time, int sleepTime)
+        {
+            int power = 700;
+            _cookController.StartCooking(power, time);
+
+            Thread.Sleep(sleepTime);
+            _userInterface.DidNotReceive().CookingIsDone();
+        }
+
     }
 }
