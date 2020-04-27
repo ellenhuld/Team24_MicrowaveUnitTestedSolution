041a57fc4bbfcaccb333fdb6f49641e7f689f9c0
Rune Matthias Hell <au550573@uni.au.dk>
2020-04-28 01:22:52 +0200

    Integrationstest step 7 påbegyndt

--- MicrowaveIntegrationTest/IntegrationTestStep7.cs
+++ MicrowaveIntegrationTest/IntegrationTestStep7.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
-using NSubstitute;
+using System.Threading;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
-
+using NUnit.Framework;
+using NSubstitute;
 
 namespace MicrowaveIntegrationTest
 {
@@ -19,56 +19,27 @@ namespace MicrowaveIntegrationTest
         private Button _timeButton;
         private Button _startCancelButton;
         private Button _powerButton;
-        private ICookController _iCookController;
         private UserInterface _userInterface;
-        private ILight _iLight;
+        private ICookController _cookController;
+        private ILight _light;
         private Display _display;
-        private IOutput _iOutput;
+        private IOutput _output;
 
+        [SetUp]
         public void SetUp()
         {
+            _cookController = Substitute.For<ICookController>();
+            _light = Substitute.For<ILight>();
+            _output = Substitute.For<IOutput>();
+
             _door = new Door();
             _timeButton = new Button();
             _startCancelButton = new Button();
             _powerButton = new Button();
-            _iCookController = Substitute.For<ICookController>();
-            _iLight = Substitute.For<ILight>();
-            _iOutput = Substitute.For<IOutput>();
-            _display = new Display(_iOutput);
-            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _iLight, _iCookController);
+            _display =new Display(_output);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
         }
 
-        [TestCase]
-        public void DisplayPower_PowerButtonPressed_PowerIsShown()
-        {
-            int power = 50;
-            _powerButton.Press();
-            string exspectedOutout = "" + power;
-            _iOutput.Received(1).OutputLine(Arg.Is<string>(s => s.Contains(exspectedOutout)));
-        }
 
-        [TestCase(01, 00)]
-        public void DisplayTime_PressTimeButton_TimeIsShown(int minute, int sec)
-        {
-            // Act
-            _powerButton.Press();
-            _timeButton.Press();
-
-            string expectedOutput = minute + ":" + sec;
-
-            //Assert
-            _iOutput.Received(1).OutputLine(Arg.Is<string>(s => s.Contains(expectedOutput)));
-
-        }
-
-        [TestCase]
-        public void CookingIsDone_WhenCookingIsDone_DisplayIsCleared()
-        {
-            _powerButton.Press();
-            _startCancelButton.Press();
-            _userInterface.CookingIsDone();
-
-            _iOutput.Received(1).OutputLine("Display cleared");
-        }
     }
 }
