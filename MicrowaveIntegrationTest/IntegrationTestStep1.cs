f236ea829456b382b592a9a6551cdfe349f9e159
Rune Matthias Hell <au550573@uni.au.dk>
2020-04-28 10:31:19 +0200

    sleeptime korrigeret i ITStep1

--- MicrowaveIntegrationTest/IntegrationTestStep1.cs
+++ MicrowaveIntegrationTest/IntegrationTestStep1.cs
@@ -31,54 +31,23 @@ namespace MicrowaveIntegrationTest
             _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
         }
 
-        // Fail compares to the order in testcases...why? alone test they succeeded
-        //[TestCase(120, 1000, 1, 59)]
-        //[TestCase(6,1000,0,5)] // fail sometimes
-        //[TestCase(120, 2000, 1, 58)]// Shouldn't fail (doesn't fail everytime)
+        // sleeptime higher because it didnt work everytime with exact time
+        [TestCase(120, 1100, 1, 59)]
+        [TestCase(6,1100,0,5)] // fail sometimes
+        [TestCase(120, 2500, 1, 58)]// Shouldn't fail (doesn't fail everytime)
         public void TimerTick_On_Display_ShowTime(int time, int sleepTime, int showMin, int showSec )
         {
             int power = 700;
             _cookController.StartCooking(power,time);
             
-            Thread.Sleep(1000);
-            _display.Received(1).ShowTime(showMin, showSec);
-        }
-
-
-
-        [TestCase(120, 1000, 1, 59)]
-        public void TimerTick_On_Display_ShowTime1(int time, int sleepTime, int showMin, int showSec)
-        {
-            int power = 700;
-            _cookController.StartCooking(power, time);
-
             Thread.Sleep(sleepTime);
             _display.Received(1).ShowTime(showMin, showSec);
         }
 
-        [TestCase(6, 1000, 0, 5)]
-        public void TimerTick_On_Display_ShowTime2(int time, int sleepTime, int showMin, int showSec)
-        {
-            int power = 700;
-            _cookController.StartCooking(power, time);
 
-            Thread.Sleep(sleepTime);
-            _display.Received(1).ShowTime(showMin, showSec);
-        }
-        [TestCase(120, 2000, 1, 58)]
-        public void TimerTick_On_Display_ShowTime3(int time, int sleepTime, int showMin, int showSec)
-        {
-            int power = 700;
-            _cookController.StartCooking(power, time);
-
-            Thread.Sleep(sleepTime);
-            _display.Received(1).ShowTime(showMin, showSec);
-        }
-        
-
-        // Again problems when it goes in the testcases
-        [TestCase(2,2000)]
-        [TestCase(4,4000)]
+        // sleeptime higher because it didnt work everytime with exact time
+        [TestCase(2,2500)]
+        [TestCase(4,4500)]
         public void Timer_Expired_CookingIsDone(int time, int sleepTime)
         {
             int power = 700;
@@ -88,17 +57,8 @@ namespace MicrowaveIntegrationTest
             _userInterface.Received(1).CookingIsDone();
         }
         [TestCase(2, 1000)]
-        [TestCase(4, 4000)]
+        [TestCase(4, 3000)]
         public void Timer_Not_Expired_No_CookingIsDone(int time, int sleepTime)
-        {
-            int power = 700;
-            _cookController.StartCooking(power, time);
-
-            Thread.Sleep(sleepTime);
-            _userInterface.DidNotReceive().CookingIsDone();
-        }
-        [TestCase(4, 4000)]
-        public void Timer_Not_Expired_No_CookingIsDone1(int time, int sleepTime)
         {
             int power = 700;
             _cookController.StartCooking(power, time);
